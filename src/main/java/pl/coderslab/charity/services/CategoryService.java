542daf21483111e4f070d44f2cd4eebe06724f88
SzymonTomalik <stomalik@gmail.com>
2021-01-03 19:40:15 +0100

    service of Category

---
+++ src/main/java/pl/coderslab/charity/services/CategoryService.java
@@ -0,0 +1,1 @@
+package pl.coderslab.charity.services;import lombok.RequiredArgsConstructor;import lombok.extern.slf4j.Slf4j;import org.springframework.stereotype.Service;@Service@Slf4j@RequiredArgsConstructorpublic class CategoryService {    private final CategoryService categoryService;}
