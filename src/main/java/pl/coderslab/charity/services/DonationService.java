ade9ea637505bddf5945cecbcee1b741e820a5ed
SzymonTomalik <stomalik@gmail.com>
2021-01-03 20:25:46 +0100

    Donation Entity&Repo&Service

---
+++ src/main/java/pl/coderslab/charity/services/DonationService.java
@@ -0,0 +1,1 @@
+package pl.coderslab.charity.services;import lombok.RequiredArgsConstructor;import lombok.extern.slf4j.Slf4j;import org.springframework.stereotype.Service;import pl.coderslab.charity.repositories.DonationRepository;@Service@Slf4j@RequiredArgsConstructorpublic class DonationService {    private final DonationRepository donationRepository;}
