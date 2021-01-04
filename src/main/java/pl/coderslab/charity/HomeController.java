0a4c3c3243017ad6e827adadebd65f6bc756c0a0
SzymonTomalik <stomalik@gmail.com>
2021-01-04 23:03:51 +0100

    HomeController updated

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -1,16 +1,31 @@
 package pl.coderslab.charity;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.services.DonationService;
+import pl.coderslab.charity.services.InstitutionService;
 
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class HomeController {
+    private final InstitutionService institutionService;
+    private final DonationService donationService;
 
 
     @RequestMapping("/")
     public String homeAction(Model model){
+//        chwilowo przekazuje tylko 4 losowe instytucje z bazy
+        model.addAttribute("institutions", institutionService.findRandomFourInstitutions());
+//        jeśli trzeba bedzie przekazac wszytskie do widoku to:
+//        model.addAttribute("institutions", institutionService.findAll());
+        model.addAttribute("quantity", donationService.countQuantityOfDonatedBags());
+        model.addAttribute("allDonations", donationService.countDonations());
+
         return "index";
     }
 }
