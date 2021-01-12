17649b0d3e7f997c055512d84bae2ad41c2db58f
SzymonTomalik <stomalik@gmail.com>
2021-01-12 19:51:30 +0100

    User repo and service

---
+++ src/main/java/pl/coderslab/charity/services/UserService.java
@@ -0,0 +1,1 @@
+package pl.coderslab.charity.services;import lombok.RequiredArgsConstructor;import org.springframework.stereotype.Service;import pl.coderslab.charity.repositories.UserRepository;@Service@RequiredArgsConstructorpublic class UserService {    private final UserRepository userRepository;}
