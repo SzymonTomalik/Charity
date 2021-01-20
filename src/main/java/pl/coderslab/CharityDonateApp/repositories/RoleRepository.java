c90156bb7499baad619a3b5ef2ad57fe8509b2cc
SzymonTomalik <stomalik@gmail.com>
2021-01-20 18:44:25 +0100

    Login/logout finished

---
+++ src/main/java/pl/coderslab/CharityDonateApp/repositories/RoleRepository.java
@@ -0,0 +1,1 @@
+package pl.coderslab.CharityDonateApp.repositories;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.stereotype.Repository;import pl.coderslab.CharityDonateApp.entities.Role;@Repositorypublic interface RoleRepository extends JpaRepository<Role, Integer> {    Role findByName(String name);}
---
+++ src/main/java/pl/coderslab/CharityDonateApp/services/CharityUserService.java
@@ -0,0 +1,1 @@
+package pl.coderslab.CharityDonateApp.services;import pl.coderslab.CharityDonateApp.entities.CharityUser;public interface CharityUserService {    CharityUser findByEmail(String email);    void addUser (CharityUser charityUser);}
